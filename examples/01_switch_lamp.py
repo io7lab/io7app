2996ec67fc2f971fe254a4cebc69b9d569d0b349
Yoonseok Hur <yoonseok@gmail.com>
2026-05-04 14:58:37 +0900

    examples: switch1 -> sw1 to match the iot201 broker's actual device id

--- examples/01_switch_lamp.py
+++ examples/01_switch_lamp.py
@@ -4,7 +4,7 @@ from io7app import App
 app = App()
 
 
-@app.on_event("switch1", "status")
+@app.on_event("sw1", "status")
 def on_switch(data):
     app.send_cmd("lamp1", "lamp", {"lamp": data["switch"]})
 
